f2694952e159ea26b4c14e9221c286595046db9d
Lucas Ginard <contactolucasginard@gmail.com>
2022-12-27 00:41:33 -0300

    init UI login, fix call service rated movies

--- app/src/main/java/com/lucasginard/pelispedia/network/APIService.kt
+++ app/src/main/java/com/lucasginard/pelispedia/network/APIService.kt
@@ -12,10 +12,9 @@ interface APIService {
     @GET("movie/popular")
     suspend fun getPopularMovies(@Query("language") language:String = Constants.LANGUAGE_DEFAULT, @Query("api_key") apiKey:String = BuildConfig.API_KEY): Response<MoviesResponse>
 
-    @GET("movie/top_rate")
+    @GET("movie/top_rated")
     suspend fun getTopRatedMovies(@Query("language") language:String = Constants.LANGUAGE_DEFAULT, @Query("api_key") apiKey:String = BuildConfig.API_KEY): Response<MoviesResponse>
 
     @GET("movie/now_playing")
     suspend fun getNowPlayingMovies(@Query("language") language:String = Constants.LANGUAGE_DEFAULT, @Query("api_key") apiKey:String = BuildConfig.API_KEY): Response<MoviesResponse>
-
 }
